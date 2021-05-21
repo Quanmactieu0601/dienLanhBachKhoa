184fd1a266b9517f4829cd4f162fa414d0fc3e8a
unknown <you@example.comnhatdieptrithu99@gmail.com>
2021-05-21 12:25:35 +0700

    khoi tao project

---
+++ src/main/java/com/cpmpany/dienLanhBachKhoa/DienLanhBachKhoaApplication.java
@@ -0,0 +1,13 @@
+package com.cpmpany.dienLanhBachKhoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DienLanhBachKhoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DienLanhBachKhoaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cpmpany/dienLanhBachKhoa/DienLanhBachKhoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cpmpany.dienLanhBachKhoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DienLanhBachKhoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
